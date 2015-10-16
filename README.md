26dabd947a6912c252153ef379bd6a12ba2497c3
rcd-github <rodrigo_carcamodiaz@yahoo.co.uk>
2015-10-16 10:50:18 -0400

    updated Oct 16 2015

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
----
-title: "README"
-output: html_document
----
+# README FILE
 
 ### Request 1: Merge the training and the test sets to create one data set
 
@@ -16,39 +13,40 @@ output: html_document
 I used *cbind()* to combine the 3 training and the 3 test files, and then i used *rbind()* to bring both
 toghether
 
+1.4 Rename the files
+I  renamed the columns containing features in the *test_train_df* data frame (i.e. all columns except for subject_id and activity_id) with the names listed in the *features* data frame. 
+
 **Outcome: the *test_train_df* data frame combines the test and training data sets**
-
-### Request 3: Use descriptive activity names to name the activities in the data set
-
-I renamed the column names of both the *test_train_df* data frame and the *activity_labels* data frame and merged them on the activity_id column, using *merge()*.
-
-**Outcome: the *big_data data* frame has descriptive activity names in the "activity" column**
-
-### Request 4: Appropriately label the data set with descriptive variable names
-
-I  renamed the columns containing features in the *big_data* data frame (i.e. all columns except for subject_id, activity_id and activity) with the names listed in the *features* data frame. 
-
-**Outcome: the *big_data* data frame has column names that describe the features and measurements as provided by the *features.txt* file**
 
 ### Request 2: Extracts only the measurements on the mean and standard deviation for each measurement
 
-I used the *grep()* function to find the column names containing "mean" and "std" and then created a subset of the *big_data* data frame containing only those columns, plus subject_id, activity_id and activity. 
+I used the *grep()* function to find the column names containing "mean" and "std" and then created a subset of the data frame containing only those columns, plus  subject_id, and activity_id. 
 
 **Outcome: the *small_data* data frame obtained includes only 66 combinations of the 33 features with each of the mean and standard deviation measurements (i.e. 66 columns)**
 
+### Request 3: Use descriptive activity names to name the activities in the data set
+
+I renamed the column names of both the *small_data* data frame and the *activity_labels* data frame and merged them on the activity_id column, using *merge()*.
+
+**Outcome: the *small_data data* frame has descriptive activity names in the "activity" column which replaces the activity_id variable**
+
+### Request 4: Appropriately label the data set with descriptive variable names
+
+I renamed the column names using the grep() and gsub() functions in order to take into account the variables described in the Code Book. 
+
+**Outcome: the small_data data frame has column names that contain the variables in the code book, but in "wide format"**
+
 ### Request 5: From the data set in step 4, creates a second, independent tidy data set              with the average of each variable for each activity and each subject
 
-* Create a tidy data set with the variables: subject_id, activity, feature,measurement, and value
+* Create a tidy data set with the variables: subject_id, activity, sensor_signal, axis_or_norm, jerk, signal_domain, statistic and value
 
-First, I used the *gather()* function to create a variable containing the 66 features/measures combinations. Second, I used the *separate()* function to obtain a column (variable) called "features" that can take 33 different values, corresponding to the features identified in the *features_info.txt* files, and another column (variable) that can take two values: mean and standard deviation, corresponding to the two measurements requested by Coursera out of the original 17 measurements present in the *features_info.txt* file. 
+First, I used the *gather()* function to create a variable containing the 66 variables to manipulate. Second, I used the *separate()* function to obtain five columns: ubject_id, activity, sensor_signal, axis_or_norm, jerk, signal_domain, statistic. These variables are explained in the code book. 
 
-Note: due to the composition of the strings in the *features* data frame, I had to make some intermediate steps to separate the two variables.
-
-**Outcome: the *tidy_data* data frame obtained contains the following variables: *subject_id* (30 possible values), *activity* (6 possible values), *feature* (33 possible values), *measurement* (i.e. either "mean" or "std"), and *values***
-
+Then I used the select() and arrange() dplyr package functions to select the variables of interest in the order I wanted them (as in the code book), and sort them by subject id. Then I used a for() loop to convert the new columns to factor. 
 
 * Group the tidy data and summarize to calculate the mean for each activity and subject
 
-I used the *group_by()* function to group the *tidy_data* data frame for each of the four variables: subject_id, activity, feature, and measurement. Then I used the *summarise()* function to calculate the means by group.
+I used the *group_by()* function to group the *tidy_data* data frame for each of the seven variables: subject_id, activity, sensor_signal, axis_or_norm, jerk, signal_domain, and statistic. Then I used the *summarise()* function to calculate the means by group.
 
-**Final Outcome: the *tidy data* frame calculates the mean (average_value) for each person, for each activity, for each feature and for each measure**
+**Final Outcome: the *tidy data* frame calculates the mean (average_value) for each person, for each activity, and for each of the five other variables: sensor_signal,
+# axis_or_norm,jerk, signal_domain and statistic**
--- run_analysis.R
+++ run_analysis.R
@@ -37,59 +37,90 @@ names(train_df) ; rm(part_1, X_train, subject_train, y_train)
 test_train_df <- rbind(test_df, train_df) %>% tbl_df()
 rm(test_df, train_df) # clean up
 
-# Outcome: the test_train_df data frame combines the test and training data sets
-
-### Request 3: Use descriptive activity names to name the activities in the data set
-
-names(test_train_df)[c(1,length(names(test_train_df)))] <- c("subject_id", "activity_id")
-names(activity_labels)[1:2] <- c("activity_id","activity")
-big_data  <- merge(test_train_df, activity_labels, all=TRUE) %>% tbl_df
-rm(test_train_df, activity_labels) # clean up
-
-# Outcome: the big_data data frame has descriptive activity names in the "activity" column
-
-### Request 4: Appropriately label the data set with descriptive variable names
-names(big_data)[3:(length(names(big_data))-1)] <- as.character((features)[,2])
+# 1.4 rename variables
+names(test_train_df)[2:(length(names(test_train_df))-1)] <- as.character((features)[,2])
 rm(features) # clean up
+names(test_train_df)[c(1,length(names(test_train_df)))] <- c("subject_id", "activity_id")
 
-# Outcome: the big_data data frame has column names that describe the features and 
-# measurements as provided by the features.txt file 
+# Outcome: the test_train_df data frame combines the test and training data sets
 
 ### Request 2: Extracts only the measurements on the mean and standard deviation 
 #             for each measurement
 
-selnames <-names(big_data)[grep("mean[^F]|std" , names(big_data))]
-small_data <- big_data[ ,c("subject_id", "activity_id", "activity",selnames)] %>% tbl_df()
-rm(selnames, big_data) # clean up
+selnames <-names(test_train_df)[grep("mean[^F]|std" , names(test_train_df))]
+small_data <- test_train_df[ ,c("subject_id", "activity_id", selnames)] %>% tbl_df()
+rm(selnames, test_train_df) # clean up
 
 # Outcome: the small_data data frame includes only 66 combinations of the 33 features 
 # with each of the mean and standard deviation measurements (i.e. total 66 variables)
 
+### Request 3: Use descriptive activity names to name the activities in the data set
+
+names(activity_labels)[1:2] <- c("activity_id","activity")
+small_data1  <- merge(small_data, activity_labels, all=TRUE) 
+small_data <- small_data1[,c(2,69,3:68)] %>% tbl_df
+rm(small_data1, activity_labels) # clean up
+
+# Outcome: the big_data data frame has descriptive activity names in the "activity" 
+# column replacing activity ids coming from the activity_labels data frame
+
+### Request 4: Appropriately label the data set with descriptive variable names
+
+values <- gsub("^t", "time-", names(small_data))
+values <- gsub("^f", "freq-", values)
+values <- gsub("BodyBody", "Body", values)
+values <- gsub("BodyGyro", "Gyro", values)
+values <- gsub("mean()", "mean", values, fixed=TRUE)
+values <- gsub("std()", "std", values, fixed=TRUE)
+values <- gsub("BodyAcc-mean", "BodyAcc-NoJerk-mean", values)
+values <- gsub("BodyAcc-std", "BodyAcc-NoJerk-std", values)
+values <- gsub("GravityAcc-mean", "GravityAcc-NoJerk-mean", values)
+values <- gsub("GravityAcc-std", "GravityAcc-NoJerk-std", values)
+values <- gsub("Gyro-mean", "Gyro-NoJerk-mean", values)
+values <- gsub("Gyro-std", "Gyro-NoJerk-std", values)
+values <- gsub("GyroMag", "Gyro-NoJerk-Euclid", values)
+values <- gsub("BodyAccMag", "BodyAcc-NoJerk-Euclid", values)
+values <- gsub("GravityAccMag", "GravityAcc-NoJerk-Euclid", values)
+values <- gsub("BodyAccJerk", "BodyAcc-Jerk", values)
+values <- gsub("GravityAccJerk", "GravityAcc-Jerk", values)
+values <- gsub("GyroJerk", "Gyro-Jerk", values)
+values <- gsub("JerkMag-mean", "Jerk-mean-Euclid", values)
+values <- gsub("JerkMag-std", "Jerk-std-Euclid", values)
+values <- gsub("Euclid-mean", "mean-Euclid", values)
+values <- gsub("Euclid-std", "std-Euclid", values)
+names(small_data) <- values ; rm(values)
+
+
+# Outcome: the small_data data frame has column names that contain the variables in
+# the codebook, but in "wide format"
+
+
 ### Request 5: From the data set in step 4, creates a second, independent tidy data set
 #              with the average of each variable for each activity and each subject
 
-# 5.1 create a tidy data set with the variables: subject_id, activity, feature,measurement,
-#     and value
-mini1 <- gather(small_data, feature_variable_axis, values, -c(subject_id, activity_id, activity))  %>% tbl_df()
-mini1$feature_variable_axis <- as.character(mini1$feature_variable_axis)
-mini1$feature_variable_axis[grep("mean..$|std..$" , mini1$feature_variable_axis)] <-
-  paste(mini1$feature_variable_axis[grep("mean..$|std..$" , mini1$feature_variable_axis)], "noaxis", sep="-")
-data <- separate(mini1, feature_variable_axis, c("feature","variable","axis"), sep="-") %>% tbl_df()
-data <- mutate(data, feature_axis=paste(feature,axis, sep="-")) %>% select(subject_id, activity, variable, feature_axis, values)
-data$feature_axis <-  sub("-noaxis$", "", data$feature_axis)
-data$variable <- sub("mean..", "mean", data$variable); data$variable <- sub("std..", "std", data$variable)
-data$variable <- as.factor(data$variable) ; data$feature_axis <- as.factor(data$feature_axis)
-tidy_data <- select(data, subject_id, activity, feature=feature_axis, measurement=variable, values) %>%
-  arrange(subject_id) 
-rm(small_data, mini1, data) # clean up
+# 5.1 create a tidy data set with the variables: subject_id, activity, sensor_signal,
+# axis_or_norm, jerk, signal_domain, statistic and value
+
+mini1 <- gather(small_data, features, values, -c(subject_id, activity))  %>% tbl_df()
+
+data <- separate(mini1, features, c("signal_domain","sensor_signal","jerk","statistic",
+                                    "axis_or_norm"), sep="-") %>% tbl_df()
+tidy_data <- select(data, subject_id, activity, sensor_signal, axis_or_norm, jerk,
+               signal_domain, statistic, values) %>% arrange(subject_id)
+for(i in names(tidy_data)[3:7]) {
+  tidy_data[[i]] <- as.factor(tidy_data[[i]])
+}
+rm(small_data, mini1, data, i) # clean up
 
 # 5.2 group the tidy data and summarize to calculate the mean for each activity and subject
 
-group_data <- group_by(tidy_data, subject_id, activity, feature, measurement) 
+group_data <- group_by(tidy_data, subject_id, activity, sensor_signal, axis_or_norm,
+                       jerk, signal_domain, statistic) 
 tidy <- summarise(group_data, average_value=mean(values, na.rm=TRUE))
 rm(group_data, tidy_data)
 
 # Outcome: the tidy data frame calculates the mean (average_value) for each person, 
-# for each activity, for each feature and for each measure. 
+# for each activity, and for each of the five other variables: sensor_signal,
+# axis_or_norm,jerk, signal_domain and statistic
 
-# write.table(tidy, "tidy.txt", row.name=FALSE)
+write.table(tidy, "tidy.txt", row.name=FALSE)
